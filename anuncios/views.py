e204c60df4386b4bc015caef2a3e22caf60a11da
Ana Luiza <aluizaribeiro1@gmail.com>
2024-12-12 22:49:06 -0300

    iniciando a configuração das views e urls da app anuncios

--- anuncios/views.py
+++ anuncios/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request,'index.html')
+
+def cadastro_anunciante(request):
+    return render (request, 'cadastro_anunciante.html')
+
+def anuncio_cadastro(request):
+    return render(request, 'anuncio_cadastro.html')
+
+def meus_anuncios(request):
+    return render(request, 'meus_anuncios.html')
--- siap/urls.py
+++ siap/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from anuncios.views import home
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', include('anuncios.urls')),
+    
 ]
