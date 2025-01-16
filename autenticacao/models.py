597c7635674b515e7c48c3e47f2ee3187145d7f7
LuisP30 <pimenta.lh2@gmail.com>
2025-01-16 16:00:25 -0300

    configuração parcial de autenticação

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -3,16 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 class Anunciante(AbstractUser):
     email = models.EmailField('E-mail', null=False, blank=False, unique=True)
-    empreendimento = models.CharField('Empreendimento', max_length=50)
     cnpj = models.CharField('CNPJ', max_length=14, null=True, blank=True, unique=True)
     endereco = models.CharField('Endereço', max_length=50, null=False)
     telefone = models.CharField('Telefone', max_length=11, null=False)
     whatsapp = models.CharField('WhatsApp', max_length=100, null=True, blank=True, unique=True)
     instagram = models.CharField('Instagram', max_length=100, null=True, blank=True, unique=True)
-    
+
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'empreendimento', 'cnpj', 'endereco', 'telefone', 'whatsapp', 'instagram']
+    REQUIRED_FIELDS = ['username', 'cnpj', 'endereco', 'telefone', 'whatsapp', 'instagram']
     
     def __str__(self):
-        return self.empreendimento
+        return self.username
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from .models import Anunciante
 
 def cadastro(request):
+    
     if request.method == 'POST':
-        pass
-    # return render(request, 'cadastro_anunciante.html')
+        
+        # Capturando dados da requisição
+        email = request.POST.get('email')
+        password = request.POST.get('senha')
+        username = request.POST.get('nome')
+        cnpj = request.POST.get('cnpj')
+        endereco = request.POST.get('endereco')
+        telefone = request.POST.get('telefone')
+        whatsapp = request.POST.get('whatsapp')
+        instagram = request.POST.get('instagram')
+        
+        # Salvando anunciante no banco de dados
+        anunciante = Anunciante.objects.create_user(
+            email=email,
+            password=password,
+            username=username,
+            cnpj=cnpj,
+            endereco=endereco,
+            telefone=telefone,
+            whatsapp=whatsapp,
+            instagram=instagram
+        )
+        return redirect('autenticacao:login')
+    return render(request, 'cadastro_anunciante.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        usuario = authenticate(request, email=email, password=senha)
+        print('teste')
+        if usuario:
+            print('Autenticado')
+            pass
     return render(request, 'login.html')
